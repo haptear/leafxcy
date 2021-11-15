3516269d1664bc85b380e31da1aef27f7f49d70c
Leaf <444653703@qq.com>
2021-11-15 11:22:52 +0800

    更新重写说明

--- blackUnique.js
+++ blackUnique.js
@@ -9,8 +9,8 @@ https://raw.githubusercontent.com/leafxcy/JavaScript/main/blackUnique.jpg
 暂不支持多账号
 
 青龙：
-捉https://pyp-api.chuxingyouhui.com/api/app/userCenter/v1/info的包，获得appId
-捉https://market.chuxingyouhui.com/promo-bargain-api/activity/mqq/api/indexTopInfo?的包，获得header
+捉https://market.chuxingyouhui.com/promo-bargain-api/activity/mqq/api/indexTopInfo?的包，获得appId
+捉https://pyp-api.chuxingyouhui.com/api/app/userCenter/v1/info的包，获得其他header
 然后填在blackJSON里面，注意按照JSON格式填写。用青龙面板的环境变量或者外面用双引号的，字符串内需要用\"转义
 export blackJSON='{"black-token":"", "token":"", "User-Agent":"", "device-value":"", "device-type":"", "phpUserId":"", "appId":""}'
 
@@ -20,7 +20,7 @@ V2P，圈X：重写方法 -- 点击右下角【我的】-> 【每日签到赚现
 0 * * * * https://raw.githubusercontent.com/leafxcy/JavaScript/main/blackUnique.js, tag=全球购骑士特权, enabled=true
 [rewrite_local]
 https://pyp-api.chuxingyouhui.com/api/app/userCenter/v1/info url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/blackUnique.js
-https://market.chuxingyouhui.com/promo-bargain-api/activity/mqq/api/indexTopInfo?  url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/blackUnique.js
+https://market.chuxingyouhui.com/promo-bargain-api/activity/mqq/api/indexTopInfo? url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/blackUnique.js
 [MITM]
 hostname = *.chuxingyouhui.com
 
